8009d3978b8e72f65b8277810215c00d080ed9dc
M Awais Hanif <mawaishanif@hotmail.com>
2017-06-04 21:41:35 +0500

    Fixed the post update date issue.

--- inc/inject-framework.php
+++ inc/inject-framework.php
@@ -17,9 +17,21 @@
 function post_data(){
 	return [
 	'date_published' =>  esc_html( get_the_date( 'c' ) ),
-	'date_modifed' => esc_html( get_the_modified_date() ),
+	'date_modifed' => esc_html( get_the_modified_date('c') ),
 	'posted_time' => esc_html( get_the_time()),
 	'author_name' => esc_html( get_the_author() ),
 	'author_image' => get_avatar(get_the_author_meta('ID'))
 	];
 }
+
+function new_excerpt_more( $more ) {
+	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
+
+
+function trimmed_excerpt( $post_excerpt, $post){
+	return $post_excerpt . '<p>Lets see</p>';
+}
+apply_filters( 'get_the_excerpt', 'trimmed_excerpt', 5 );
