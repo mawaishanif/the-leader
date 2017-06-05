0225330d004919489e54cd119075bccdecbc7ba0
Tayyab Hanif <mrtayyabhanif@gmail.com>
2017-06-05 17:32:56 +0500

    Updated Header - With responsive Navigation

--- header.php
+++ header.php
@@ -34,16 +34,16 @@
 				<span class="close_search"><span class="icon ti-close"></span></span>
 			</div>
 			<div class="main-menu-container  row between-sm  between-md  between-lg  middle-xs  middle-sm  middle-md  middle-lg  center-xs center-sm center-md center-lg full-width">
-				<div class="brand col-xs-12 col-sm-2 col-md-2 col-lg-2">
+				<div class="brand col-xs-4 col-sm-2 col-md-2 col-lg-2">
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 						<h5 class="site-title"><?php bloginfo( 'name' ); ?></h5>
 					</a>
 				</div>
-				<nav  id="site-navigation" role="navigation" class="main col-xs-12 col-sm-10 col-md-8 col-lg-8">
+				<nav  id="site-navigation" role="navigation" class="main end-md end-lg col-xs-4 col-sm-9 col-md-9 col-lg-9">
 					<div id="menu-trigger"> <a href="#" class="menu-heading">MENU <span class="icon ti-menu"></span></a> </div>
 					<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav>
-				<div class="search-menu col-sm-2 col-md-2 col-lg-2">
+				<div class="search-menu col-xs-4 col-sm-1 col-md-1 col-lg-1">
 					<ul class="align-right search">
 						<li><a href="#0" class="close_search">
 							<span class="icon ti-search"></span>
