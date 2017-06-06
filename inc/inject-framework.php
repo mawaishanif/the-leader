643996246656b2795ff997e0d03141307976c0e9
M Awais Hanif <mawaishanif@hotmail.com>
2017-06-06 21:43:57 +0500

    Added new function for adding template specific style sheet into the page

--- inc/inject-framework.php
+++ inc/inject-framework.php
@@ -8,17 +8,21 @@
  *
  */
 
-
 /**
- * Gets the post meta related data
- * @param none
+ * Retrive the data for image used in wordpress loop
+ * @param int $image_id 
+ * @param string $image_thumb_name 
  * @return array
  */
-
 function image_data($image_id, $image_thumb_name){
 	return wp_get_attachment_image_src( $image_id, $image_thumb_name );
 }
 
+
+/**
+ * Returns the post meta data in form of array
+ * @return array
+ */
 function post_data(){
 
 	return [
@@ -30,13 +34,49 @@ function post_data(){
 	];
 }
 
-
+/**
+ * Appends at the end of excerpt
+ * @param string $more 
+ * @return string
+ */
 function the_leader_excerpt_more( $more ) {
 	return '...';
 }
 add_filter( 'excerpt_more', 'the_leader_excerpt_more' );
 
+/**
+ * Adds a length limit to excerpt
+ * @param int $length 
+ * @return int
+ */
 function the_leader_custom_excerpt_length( $length ) {
     return 25;
 }
 add_filter( 'excerpt_length', 'the_leader_custom_excerpt_length', 999 );
+
+/**
+ * Checks weather a template part file is being used on a page or not
+ * @param string $url 
+ * @return bool
+ */
+function check_template_part( $url ){
+
+	$all_templates_used = get_included_files();
+
+	$directory_path = str_replace('/', '\\', get_template_directory());
+
+	$url = '\\' . str_replace('/', '\\', $url) . '.php';
+
+	$is_template =  array_search($directory_path . $url, $all_templates_used);
+
+	if ($is_template) {
+
+		return True;
+
+	}
+	else {
+
+		return False;
+
+	}
+}
