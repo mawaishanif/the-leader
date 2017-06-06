442f8e70b9e985aa21f1af90d5a498c39ca0189f
M Awais Hanif <mawaishanif@hotmail.com>
2017-06-06 23:50:53 +0500

    Added another way to upload style files based on template parts

--- functions.php
+++ functions.php
@@ -147,9 +147,9 @@ function the_leader_scripts() {
 	/**
 	 * This is an example demonstrating how the page template part specific styles could be loaded.
 	 **/
-	if (check_template_part('layouts/header-style-broad')) {
-		wp_enqueue_style( 'header-style-broad', get_template_directory_uri() . '/assets/css/headers/header-style-broad.css');
-	}
+
+	wp_enqueue_style( 'header-style-broad', get_template_directory_uri() . '/assets/css/headers/header-style-broad.css');
+
 	wp_enqueue_script( 'the_leader-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'the_leader-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
